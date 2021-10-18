0fde07095d0d1c3d1522fcfd7863b8eea62ddd16
Luis Alfredo Porras Paez <luis@webdox.cl>
2021-10-18 09:47:44 -0300

    WIP adding posts endpoints, testing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Post, type: :model do
     it "validate presence required fields" do
       should validate_presence_of(:title)
       should validate_presence_of(:content)
-      should validate_presence_of(:published)
       should validate_presence_of(:user_id)
     end
   end
